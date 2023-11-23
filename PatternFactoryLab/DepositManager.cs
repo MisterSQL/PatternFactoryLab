8ee7cc1cc0fd067084cc1a596819f405cc6220cd
Suren Poghosyan <dendy20123@gmail.com>
2023-11-22 20:41:11 -0800

    Добавьте файлы проекта.

---
+++ PatternFactoryLab/DepositManager.cs
@@ -0,0 +1,13 @@
+﻿ namespace PatternFactoryLab
+{
+        // This class represents a Deposit Manager, implementing the IBankManager interface.
+    class DepositManager : IBankManager
+    {
+        // Implementation of the processClientRequest method specific to deposit management.
+        public void processСlientRequest()
+        {
+        //Output a message indicating that deposit requests are being processed.
+          Console.WriteLine("Processing deposit requests");
+        }
+    }
+}
