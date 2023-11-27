e13ca406fcb54b5131686e36c93eb88620d90ad2
Suren Poghosyan <dendy20123@gmail.com>
2023-11-26 20:06:07 -0800

    revert changes

--- PatternFactoryLab/BankManager.cs
+++
@@ -1,9 +0,0 @@
-﻿ namespace PatternFactoryLab
-{
-    // This interface defines the contract for bank managers.
-    interface IBankManager
-    {
-        // Method to process client requests.
-        void processСlientRequest();
-    }
-}
